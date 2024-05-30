3e2f33ca773adf1e34c93d65fb6b0f0e5dfad234
Mathieu DELFOSSE <mat.delfosse@gmail.com>
2024-05-30 12:17:05 +0200

    quete 4

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -6,21 +6,21 @@ const { sayWelcome } = require("../../controllers/sayActions");
 
 router.get("/", sayWelcome);
 
-const { browse } = require("../../controllers/programActions");
-
-router.get("/", browse);
-
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
+const categoriesRouter = require("./categories/router");
 
-router.use("/items", itemsRouter);
+router.use("/categories", categoriesRouter);
 
 const programsRouter = require("./programs/router");
 
 router.use("/programs", programsRouter);
+
+const itemsRouter = require("./items/router");
+
+router.use("/items", itemsRouter);
 
 /* ************************************************************************* */
 
